388e8b72928c330ac934ae74858ff6a80b7ea39c
IndieX968 <appm4196@gmail.com>
2025-03-22 11:06:38 +0500

    Update API and Socket.IO URLs to production endpoints

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,6 @@
-export const sokitIoURL = "http://localhost:5000";
+export const sokitIoURL = "https://backend-main-zeta.vercel.app";
 //http://localhost:5000
 //https://backend-main-zeta.vercel.app
-export const baseUrl = "http://localhost:5000/api";
+export const baseUrl = "https://backend-main-zeta.vercel.app/api";
 // "https://backend-main-zeta.vercel.app/api"
 // "http://localhost:5000/api"
